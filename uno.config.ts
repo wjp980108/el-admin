b57d8a728eb9dfd8b93a7b4bd93596923fdd76c4
wjp <wjp_github@163.com>
2024-05-21 18:08:23 +0800

    初始化项目

---
+++ uno.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  shortcuts: {
+    'wh-full': 'w-full h-full',
+    'flex-center': 'flex justify-center items-center',
+    'flex-col-center': 'flex-center flex-col',
+    'flex-x-center': 'flex justify-center',
+    'flex-y-center': 'flex items-center',
+  }
+})
