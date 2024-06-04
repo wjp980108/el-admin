fc1cf714dba0b24276912a6d9aae1070a90d006e
wjp980108 <54931083+wjp980108@users.noreply.github.com>
2024-06-04 16:31:01 +0800

    ci: commitlint配置
    新增coomitlint配置

---
+++ .commitlintrc.cjs
@@ -0,0 +1,45 @@
+// .commitlintrc.js
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+  },
+  prompt: {
+    messages: {
+      type: '选择你要提交的类型 :',
+      scope: '选择一个提交范围（可选）:',
+      customScope: '请输入自定义的提交范围 :',
+      subject: '填写简短精炼的变更描述 :\n',
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      footer: '列举出禅道中的bug编号（可选）。 例如: #31, #34:\n',
+      confirmCommit: '是否提交commit ?'
+    },
+    types: [
+      { value: 'feat', name: 'feat:     新增功能 | A new feature' },
+      { value: 'fix', name: 'fix:      修复缺陷 | A bug fix' },
+      {
+        value: 'style',
+        name: 'style:    代码格式 | Changes that do not affect the meaning of the code'
+      },
+      {
+        value: 'refactor',
+        name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature'
+      },
+      {
+        value: 'test',
+        name: 'test:     测试相关 | Adding missing tests or correcting existing tests'
+      },
+      {
+        value: 'build',
+        name: 'build:    构建相关 | Changes that affect the build system or external dependencies'
+      },
+      { value: 'revert', name: 'revert:   回退代码 | Revert to a commit' },
+      {
+        value: 'chore',
+        name: 'chore:    其他修改 | Other changes that do not modify src or test files'
+      }
+    ],
+    useEmoji: false
+  }
+}
