4d191a269e037c4822cabbc82e88c47a503f09b6
wjp980108 <54931083+wjp980108@users.noreply.github.com>
2024-06-05 09:23:04 +0800

    Keep calm and commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
@@ -9,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="h-100 color-inherit flex w-0">wjp</div>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
