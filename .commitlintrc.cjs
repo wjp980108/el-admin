6b26901feb2cd8e619c67b8be2b3d4cbaa87f08b
wjp980108 <54931083+wjp980108@users.noreply.github.com>
2024-06-05 17:09:24 +0800

    ci(config): 新增代码提交规范

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     // @see: https://commitlint.js.org/#/reference-rules
   },
+  extends: ['@commitlint/config-conventional'],
   prompt: {
     messages: {
       type: '选择你要提交的类型 :',
