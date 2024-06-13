ff461ff3972ea9b6623029abab4d1acc9b362edf
wjp980108 <54931083+wjp980108@users.noreply.github.com>
2024-06-13 10:16:35 +0800

    feat(pages): dqwd

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">
-          Home4
+          Home
         </RouterLink>
         <RouterLink to="/about">
           About
