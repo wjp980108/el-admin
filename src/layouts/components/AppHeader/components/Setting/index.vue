b2fc6d962bade1cd326d69491c4a6e1e61566d7f
wjp <wjp_github@163.com>
2024-06-17 17:03:05 +0800

    feat(pages): dwqd

--- src/layouts/components/AppHeader/components/Setting/index.vue
+++ src/layouts/components/AppHeader/components/Setting/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import AppWrapper from '@/layouts/components/AppHeader/components/Wrapper/index.vue'
+import { useAppStore } from '@/stores'
 
 defineOptions({ name: 'AppSetting' })
+
 const showSetting = ref(false)
+
+const appStore = useAppStore()
 </script>
 
 <template>
@@ -18,7 +22,14 @@ const showSetting = ref(false)
     <n-drawer v-model:show="showSetting" :width="360" resizable>
       <n-drawer-content title="系统设置" closable>
         <n-divider>主题设置</n-divider>
-
+        <n-space align="center" justify="space-between">
+          侧边菜单字体
+          <n-input-number v-model:value="appStore.menuFontSize" class="w-10em" :min="12" :max="24" :step="2" button-placement="both" placeholder="字体大小">
+            <template #suffix>
+              px
+            </template>
+          </n-input-number>
+        </n-space>
         <template #footer>
           <n-button>Footer</n-button>
         </template>
--- src/stores/app/index.ts
+++ src/stores/app/index.ts
@@ -3,9 +3,16 @@ import { useReset } from '@/hooks'
 const docEle = ref(document.documentElement)
 const { isFullscreen, toggle } = useFullscreen(docEle)
 
+interface AppState {
+  // 侧边栏是否折叠
+  collapsed: boolean
+  // 菜单字体大小
+  menuFontSize: number
+}
 export const useAppStore = defineStore('app', () => {
-  const [state] = useReset({
+  const [state] = useReset<AppState>({
     collapsed: false,
+    menuFontSize: 14,
   })
   // 全屏状态
   const fullscreen = computed(() => isFullscreen.value)
