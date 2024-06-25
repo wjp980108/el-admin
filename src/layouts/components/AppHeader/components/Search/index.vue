2c11105827c085370689338654022c3251a69f19
wjp <wjp_github@163.com>
2024-06-25 18:22:47 +0800

    feat(components): 新增 SearchMenu 搜索菜单组件

--- src/layouts/components/AppHeader/components/Search/index.vue
+++ src/layouts/components/AppHeader/components/Search/index.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
 import AppWrapper from '@/layouts/components/AppHeader/components/Wrapper/index.vue';
+import SearchMenu from '@/layouts/components/AppHeader/components/SearchMenu/index.vue';
 
 defineOptions({ name: 'AppSearch' });
+
+const show = ref(false);
+
+function handleSearch() {
+  show.value = true;
+}
 </script>
 
 <template>
   <div>
     <n-tooltip>
       <template #trigger>
-        <AppWrapper>
+        <AppWrapper @click="handleSearch">
           <app-icon icon="icon-park-outline:search" />
         </AppWrapper>
       </template>
       <span>搜索</span>
     </n-tooltip>
+    <SearchMenu v-model:show="show" />
   </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,13 @@ export default defineConfig({
     'flex-col-center': 'flex-center flex-col',
     'flex-x-center': 'flex justify-center',
     'flex-y-center': 'flex items-center',
+    'commands': 'h-5.5 w-5.5 flex-center b-rd-0.5 bg-[rgba(125,125,125,0.1)] text-2.5 color-#909399 shadow-commands',
   },
+  rules: [
+    ['shadow-commands', {
+      'box-shadow': 'inset 0 -2px 0 0 #cdcde6, inset 0 0 1px 1px #fff, 0 1px 2px 1px rgba(30, 35, 90, 0.4)',
+    }],
+  ],
   transformers: [
     transformerVariantGroup(),
   ],
